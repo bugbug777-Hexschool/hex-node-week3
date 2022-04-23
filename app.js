c03b26b8b003138082d51ecc3cb316f6403073db
sihle <gubug777@gmail.com>
2022-04-23 14:22:24 +0800

    add env config

--- app.js
+++ app.js
@@ -3,13 +3,20 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 const mongoose = require('mongoose');
+const dotenv = require('dotenv');
 const cors = require('cors');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 const postsRouter = require('./routes/posts');
 
-mongoose.connect('mongodb://localhost:27017/testExpress').then((res) => {
+dotenv.config({path: './.env'});
+const uri = process.env.DB_URI.replace(
+  '<password>',
+  process.env.DB_SECRET
+)
+
+mongoose.connect(uri).then((res) => {
   console.log('資料庫連線成功');
 })
 var app = express();
